4dc3b46d19c17f31de284fef7ec33c91618157ea
KevinStock <kevin@kevinstock.net>
2016-09-04 20:04:35 -0500

    converted to hmac with secret

--- main.py
+++ main.py
@@ -17,10 +17,12 @@
 import os
 import webapp2
 import jinja2
-import hashlib
+import hmac
+
+SECRET = "imsosecret"
 
 def hash_str(s):
-    return hashlib.md5(s).hexdigest()
+    return hmac.new(SECRET, s).hexdigest()
 
 def make_secure_val(s):
     return "%s|%s" % (s, hash_str(s))
